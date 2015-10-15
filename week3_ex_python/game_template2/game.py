164037a96bfd93f17f2c8b209c85600bb63aa446
Dean Bale <dean@baleuk.org>
2015-10-15 11:15:53 +0100

    renamed parser file

--- week3_ex_python/game_template2/game.py
+++ week3_ex_python/game_template2/game.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-# from map import rooms
+from map import rooms
 from player import *
 from items import *
-from parser import *
+from parser_game import *
 
 
 def list_of_items(items):
